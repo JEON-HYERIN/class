269c8962cd5e4e26db36a2310896f04944d5c1e8
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2022-01-09 13:08:26 +0900

    배열반복문

--- 배열/main.js
+++ 배열/main.js
@@ -29,3 +29,48 @@ var newNum = num.map(function (el) {
   return el * 2;
 });
 console.log(newNum);
+
+/*
+유사배열
+- 마치 배열처럼 인덱스로 프로퍼티 값에 접근할 수 있고 length 프로퍼티를 갖는 객체
+
+Array.from
+- 유사배열 객체를 인수로 전달받아서 배열로 변환하여 반환해줌
+*/
+
+var btns = document.querySelectorAll('ul li');
+console.log(btns)
+
+var btns_arr = Array.from(btns);
+console.log(btns_arr);
+
+// 유사배열(DOM)을 반복 돌면서 이벤트 연결
+btns.forEach(function (el, index) {
+  el.onclick = function () {
+    console.log(index);
+  };
+});
+
+btns_arr.map(function (el, index) {
+  console.log(el);
+});
+
+// es6 for of문
+/*
+for (let el of 반복그룹) {
+  //실행코드
+}
+*/
+
+var names = ['David', 'Andy', 'Tom'];
+var num = 0;
+for (var el of names) {
+  console.log(el);
+  console.log(num);
+  num++;
+}
+
+var text = 'TYPOGRAPHY';
+for(var a of text) {
+  console.log(a);
+}
