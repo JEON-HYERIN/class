69769a9daf84ea938cadfe5b92ae9c3ca077aa21
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2022-01-09 15:17:38 +0900

    호이스팅방지

--- domscript/main.js
+++ domscript/main.js
@@ -1,22 +1,58 @@
 /*
-속성 : Attribute
+변수의 유효범위 (variable scope)
 
-속성값을 구할 때 
-getAttribute('속성명')
+1. 전역변수(window)
+- 코드블록 밖에서 선언된 변수
+- 코드 전체에 걸쳐서 접근 가능
+- 여러개의 다른 함수들이 같은 값을 공유해야 할 때
+- 전역번수의 오염을 막기 위해 꼭 필요할 때만 생성
+- window 객체에 저장
 
-속성값을 바꿀 때
-setAttribute('변경할 속성명', '변경할 속성값')
+2. 지역변수
+- 코드블록 안에서 선언된 변수
+- 해당 코드블록 안에서만 접근 가능
+- 특정 변수의 값이 해당 코드블록 안에서만 전용으로 쓰여야 할 때 
 
-텍스트 변경
-요소.innerText = '변경할 텍스트'
+3. 호이스팅
+- 블록안에서 선언된 지역변수가 코드블록 밖으로 끌어올라가지면서 강제로 전역변수화 되는 현상
+
+즉시실행함수
+- 자기자신을 정의하자마자 바로 호출하는 함수 형태
+(함수정의)(함수호출);
+- 안쪽의 코드를 캡슐화
+- 안쪽 코드의 변수값을 초반에 한꺼번에 변경해야 될 때
+
+반복문, 조건문 안에서 지역변수를 var로 선언하면 호이스팅이 발생해서 지역변수가 전역으로 등록
 */
 
-var a = document.querySelector('a');
-var result = a.getAttribute('title');
-console.log(result);
+var lis = document.querySelectorAll('li');
+// 즉시실행함수로 지역변수 i값이 외부로 호이스팅 되는 걸 방지
+// for (var i = 0; i < lis.length; i++) {
+//   (function (index) {
+//     lis[index].onclick = function () {
+//       console.log(index);
+//     }
+//   })(i);
+// }
 
-a.setAttribute('href', 'https://nate.com');
-a.setAttribute('title', '네이트로 이동');
+// let (es6)
+for (let i = 0; i < lis.length; i++) {
+  (function (index) {
+    lis[index].onclick = function () {
+      console.log(index);
+    }
+  })(i);
+}
+// console.log(i)
 
-console.log(a.innerText);
-a.innerText = 'NATE';
+// 자기자신에게 jQuery 객체를 인수로 전달해서 내부의 모든 $기호를 jQuery로 변경하는 즉시실행함수
+// jQuery('.box)
+// (function($) {
+//   // jQuery('.box')
+//   $('.box')
+// })(jQuery)
+
+// 자기자신에게 인수 2,3을 전달해서 자체적으로 해당값들을 더하는 즉시실행함수
+(function plus (num1, num2) {
+  console.log(num1 + num2);
+})(2,3);
